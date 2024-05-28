2c9b3f5a036bc0e8504bb243f9a475b72788a024
Oleh Bilyk <olehbilyk13@gmail.com>
2024-05-28 11:54:42 +0300

    topic 1_olehbilyk

---
+++ oleh.bilyk.HW1.js
@@ -0,0 +1,83 @@
+// Task 2
+/*
+let last_name = 'Bilyk'
+
+console.log(last_name);
+*/
+
+// Task 3
+/*
+let name = " 'Oleh is' ";
+let age = 20;
+
+name = 'Ihor';
+name = age;
+
+console.log(name);
+console.log(age);
+*/
+
+// Task 4
+/*
+const user = {
+    city: "Lviv",
+    age: 20,
+    newhire: true,
+    email: null,
+    other: undefined
+}
+*/
+
+// Task 5
+/*
+let isAdult = prompt('How old are you?','age');
+
+console.log(isAdult)
+*/
+
+// Task 6
+/*
+let name = 'Oleh'
+    last_name = 'Bilyk'
+    study_group = 'QC'
+    year_of_birth = 1991
+    ismarried = true
+    email = null
+    other = undefined
+
+console.log(typeof year_of_birth) 
+console.log(typeof ismarried)
+console.log(typeof last_name)
+*/
+
+// Task 7
+/*
+
+let login = prompt("What's' your login");
+let email = prompt("Write the email");
+let password = prompt("Type your password");
+alert("Dear " + login +
+    ", your email is " + email +
+    ", your password is " +  password);
+*/
+
+// Task 8
+/*
+let hour = 60 * 60
+    day = 24 * 60 * 60
+    month_31_days = 31 * 24 * 60 * 60
+    month_30_days = 30 * 24 * 60 * 60
+    month_29_days = 29 * 24 * 60 * 60
+    month_28_days = 28 * 24 * 60 * 60
+
+console.log(hour + " seconds")
+console.log(day + " seconds")
+console.log(month_31_days + " seconds")
+console.log(month_30_days + " seconds")
+console.log(month_29_days + " seconds")
+console.log(month_28_days + " seconds")
+*/
+
+
+
+
