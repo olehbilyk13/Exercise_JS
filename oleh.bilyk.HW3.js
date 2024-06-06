4d389a034d7f8f85026c1a69640f21e64fe79a51
Oleh Bilyk <olehbilyk13@gmail.com>
2024-06-06 09:53:12 +0300

    topic 3_olehbilyk

--- oleh.bilyk.HW3.js
+++ oleh.bilyk.HW3.js
@@ -1,9 +1,115 @@
+//Task 1
+/*
+const arr = [2,3,4,5];
+let product = 1;
+    for(let i = 0; i < arr.length; i++){
+    product *= arr[i];
+ }
+    console.log(product);
 
+product = 1;
+let i = 0;
+    while (i < arr.length){
+    product *=arr[i];
+    i++;
+ }
+     console.log(product);
+*/
 
-let sum = 0;
-while (true) {
-let value = +prompt("Введіть число", '');
-if (!value) break; // (*)
-sum += value;
+// Task 2
+/*
+for (let i = 0; i <= 15; i++) {
+    if (i % 2 === 0) {
+        console.log(`${i} is even`);
+    } else {
+        console.log(`${i} is odd`);
+    }
 }
-alert( 'Сума: ' + sum );
+*/
+
+// Task 3 
+/*
+function randArray(k) {
+    let random_integers = [];
+    for(let i = 0; i < k; i++){
+        random_integers.push(Math.floor(Math.random() * 500) + 1);
+}
+        return random_integers;
+}
+
+    console.log(randArray(5));
+*/ 
+    
+// Task 4
+/*
+function raiseToDegree(a, b) {
+    return Math.pow(a, b);
+}
+
+let a = parseInt(prompt("Enter the first number"));
+let b = parseInt(prompt("Enter the second number"));
+
+console.log(raiseToDegree(a,b));
+*/
+
+// Task 5
+/*
+function findMin() {
+    let min = arguments[0];
+    for (let i = 1; i < arguments.length; i++) {
+        if (arguments[i] < min) {
+            min = arguments[i];
+        }
+    }
+    return min;
+}
+
+console.log(findMin(12, 14, 4, -4, 0.2));
+*/
+
+// Task 6
+/*
+function findUnique(arr) {
+    let uniqueElements = {};
+    for(let i = 0; i < arr.length; i++) {
+    if (uniqueElements[arr[i]])    
+{
+    return false;
+} 
+    else {
+    uniqueElements[arr[i]] = true;
+}    
+}
+    return true;
+}
+
+console.log(findUnique([1, 2, 3, 5, 3]));
+console.log(findUnique([1, 2, 3, 5, 11]));
+*/
+// Task 7
+/*
+function lastElement(array, i=1) {
+    if (i >= array.length) {
+        return array;
+    } else {
+        return array.slice(-i);
+    }
+}
+
+console.log(lastElement([3, 4, 10, -5]));
+console.log(lastElement([3, 4, 10, -5], 2));
+console.log(lastElement([3, 4, 10, -5], 8));
+*/
+
+// Task 8 
+/*
+const cap_letter = function (inf) {
+
+    let str = inf.split(" ");
+    for (let i = 0; i < str.length; i++){
+        str[i] = str[i][0].toUpperCase() + str[i].substring(1);   
+    }
+return str.join(" ");
+}
+console.log(cap_letter("i love java script"));
+*/
